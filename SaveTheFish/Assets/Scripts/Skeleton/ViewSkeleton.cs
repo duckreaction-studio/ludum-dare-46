2bde07ca21ee3dc84a3f49ce5d05fad02fdbeb0d
Matthieu Giraud <matthieu.giraud99@gmail.com>
2020-04-18 18:06:36 +0200

    Add script to view skeleton

---
+++ SaveTheFish/Assets/Scripts/Skeleton/ViewSkeleton.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Skeleton
+{
+    public class ViewSkeleton : MonoBehaviour
+    {
+
+        public Transform rootNode;
+        public Transform[] childNodes;
+        public float greenCubeScale = 0.05f;
+        public float blueCubeScale = 0.01f;
+
+        void OnDrawGizmosSelected()
+        {
+            if (rootNode != null)
+            {
+                if (childNodes == null || childNodes.Length == 0)
+                {
+                    //get all joints to draw
+                    PopulateChildren();
+                }
+
+
+                foreach (Transform child in childNodes)
+                {
+
+                    if (child == rootNode)
+                    {
+                        //list includes the root, if root then larger, green cube
+                        Gizmos.color = Color.green;
+                        Gizmos.DrawCube(child.position, Vector3.one * greenCubeScale);
+                    }
+                    else
+                    {
+                        Gizmos.color = Color.blue;
+                        Gizmos.DrawLine(child.position, child.parent.position);
+                        Gizmos.DrawCube(child.position, Vector3.one * blueCubeScale);
+                    }
+                }
+
+            }
+        }
+
+        public void PopulateChildren()
+        {
+            childNodes = rootNode.GetComponentsInChildren<Transform>();
+        }
+    }
+}
