dafba1d7f246dca90f49f5f1166557ca600b14a0
Simon <simongirau@gmail.com>
2020-04-19 03:10:45 +0200

    tweak script dof

---
+++ SaveTheFish/Assets/Scripts/Post Process/FocusDof.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class FocusDof : MonoBehaviour
+{
+    public GameObject focusTarget;
+
+    DepthOfField dofLayer = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PostProcessVolume volume = gameObject.GetComponent<PostProcessVolume>();
+        volume.profile.TryGetSettings(out dofLayer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dofLayer.focusDistance.value = Vector3.Distance(focusTarget.transform.position, Camera.main.transform.position);
+    }
+}
