243fc6c2c25bd7852ae28ef2b47411c3b5d9bc22
Simon <simongirau@gmail.com>
2020-04-19 02:07:01 +0200

    fish ik done

---
+++ SaveTheFish/Assets/Scripts/FishBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishBehaviour : MonoBehaviour
+{
+    public SkinnedMeshRenderer fishSkin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fishSkin.SetBlendShapeWeight(3, Mathf.SmoothStep(70f, 0, Mathf.PingPong(Time.time, 0.85f)));
+    }
+}
